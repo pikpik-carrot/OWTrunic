a7e1875a734169ab7340e5e2b5486c38f1354ae1
pikpik-carrot <100093626+pikpik-carrot@users.noreply.github.com>
2024-05-09 19:00:22 -0500

    first push

---
+++ OWTrunic.cs
@@ -0,0 +1,24 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using UnityEngine.Assertions;
+
+namespace OWTrunic
+{
+    public class OWTrunic : ModBehaviour
+    {
+        private void Awake()
+        {
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        private void Start()
+        {
+            var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
+            api.RegisterLanguage(this, "Trunic", "Assets/Translation.xml");
+            api.AddLanguageFont(this, "Trunic", "Assets/tunic", "Assets/Tunic.otf");
+        }
+    }
+
+}
