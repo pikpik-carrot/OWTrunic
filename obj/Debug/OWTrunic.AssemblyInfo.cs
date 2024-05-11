815754a0569d521a30260ba29a64f94a7b8335b3
pikpik-carrot <100093626+pikpik-carrot@users.noreply.github.com>
2024-05-10 19:22:53 -0500

    did some more lines

--- obj/Debug/OWTrunic.AssemblyInfo.cs
+++ obj/Debug/OWTrunic.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2024 pikpik_carrot")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7e1875a734169ab7340e5e2b5486c38f1354ae1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1cd8c0b96715615ed204660381914dd37c8d33b")]
 [assembly: System.Reflection.AssemblyProductAttribute("OWTrunic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OWTrunic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
